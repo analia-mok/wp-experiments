7bf363a17cb3392960aa28c757c79a68329a8da4
Analia Mok <analiam319@gmail.com>
2019-02-24 18:40:24 -0500

    Started plugin creation

---
+++ wp-content/plugins/cpt-registrator/class-cpt-registrator.php
@@ -0,0 +1,26 @@
+<?php
+
+class CPTRegistrator
+{
+    /**
+     * Method to run on plugin activation.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public static function activate()
+    {
+        // Nothing...
+    }
+
+    /**
+     * Method to run on plugin deactivation.
+     *
+     * @return void
+     * @since 1.0.0
+     */
+    public static function deactivate()
+    {
+        // Nothing...
+    }
+}
---
+++ wp-content/plugins/cpt-registrator/cpt-registrator.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           CPT_Registrator
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CPT Registrator
+ * Plugin URI:        http://example.com/cpt-registrator-uri/
+ * Description:       Plugin framework for auto-registering and defining custom post types.
+ * Version:           1.0.0
+ * Author:            Analia Mok
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cpt-registrator
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ */
+define( 'CPT_REGISTRATOR_VERSION', '1.0.0' );
+
+/**
+ * Execute plugin activation actions.
+ *
+ * @return void
+ */
+function activate_cpt_registrator() {
+	require_once plugin_dir_path( __FILE__ ) . 'class-cpt-registrator.php';
+	cpt_registrator_Activator::activate();
+}
+
+/**
+ * Execute plugin deactivation actions.
+ *
+ * @return void
+ */
+function deactivate_cpt_registrator() {
+	require_once plugin_dir_path( __FILE__ ) . 'class-cpt-registrator.php';
+	cpt_registrator_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_cpt_registrator' );
+register_deactivation_hook( __FILE__, 'deactivate_cpt_registrator' );
+
+/**
+ * The core plugin class.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-cpt-registrator.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * @since    1.0.0
+ */
+function run_cpt_registrator() {
+	$plugin = new cpt_registrator();
+	$plugin->run();
+}
+run_cpt_registrator();
